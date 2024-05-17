1c802ca7589fc6eab8fb7019d45b59517c53d658
RajvardhanDhakar <rajvardhandhakar1@gmail.com>
2024-05-17 10:16:59 +0530

    Create tut5BasicinputOutput.cpp

---
+++ tut5BasicinputOutput.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+using namespace std;
+
+int main()
+
+{
+    int num1, num2;
+    cout<<"Enter the value of num1: "<<endl;  //  '<<' is called insertion operator;
+    cin>>num1; //  '>>' is called extraction operator;
+    cout<<"Enter the value of num2: "<<endl;
+    cin>>num2; 
+    cout<<"the sum is " << num1+num2;
+
+}
